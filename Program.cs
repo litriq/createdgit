1354bd701b68e648fec2beadf5328803c31c7838
litriq <119163655+litriq@users.noreply.github.com>
2023-04-23 19:41:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace calcalator4ik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0; int num2 = 0;
+
+            Console.WriteLine("Калькулялька на сишарке C#\r");
+            Console.WriteLine("------------------------\n");
+
+            Console.WriteLine("Введите число и нажмите Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число и нажмите Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - плюс");
+            Console.WriteLine("\ts - минус");
+            Console.WriteLine("\tm - звездочка");
+            Console.WriteLine("\td - разделить");
+            Console.Write("Your option? ");
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Получаеца {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Получаеца {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Получаеца {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Получаеца {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            Console.Write("Нажмите любую клавишу, чтобы завершить работу кальки");
+            Console.ReadKey();
+        }
+    }
+}
